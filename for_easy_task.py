c13653ab5a197682ac35258e670741ff6f795912
twsomt <volk0v-ak@yandex.ru>
2022-05-03 01:42:32 +0300

    another set task

--- for_easy_task.py
+++ for_easy_task.py
@@ -1,8 +1,12 @@
 x = [[1, 5], [3, 4], [2, 0]]
-print(any(2 in i for i in x))
-#test
+# print(any(2 in i for i in x))
+# test
 
 
 myset = set('ъъ эээ юююю яяяяя')
-print(len(myset))
-print(myset)
+# print(len(myset))
+# print(myset)
+
+numbers = {9089, -67, -32, 1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111, 111, 1, 23}
+for i in numbers:
+    print(i)
