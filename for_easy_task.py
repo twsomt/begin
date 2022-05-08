e785ed844854ed28dc4539aea51e77902400e1ea
twsomt <volk0v-ak@yandex.ru>
2022-05-08 17:18:52 +0300

    lambda function

--- for_easy_task.py
+++ for_easy_task.py
@@ -1,16 +1,21 @@
-x = 'Кошка'
-def false_translate(x):
-    return x[::-1].capitalize()
+# x = 'Кошка'
+# def false_translate(x):
+#     return x[::-1].capitalize()
 
-print(false_translate(x))
+# print(false_translate(x))
 
-set1 = {'Yellow', 'Orange', 'Black'}
-set2 = {'Orange', 'Blue', 'Pink'}
+# set1 = {'Yellow', 'Orange', 'Black'}
+# set2 = {'Orange', 'Blue', 'Pink'}
 
-set1.difference_update(set2)
-print(*set1)
+# set1.difference_update(set2)
+# print(*set1)
 
-x = {'a', 'b'}
-print(x * 2)
+# x = {'a', 'b'}
+# print(x * 2)
 
 # invisible commit
+
+capitals = {'Россия': 'Москва', 'Англия': 'Лондон', 'Чехия': 'Прага', 'Бразилия':'Бразилиа'}
+
+for key, value in sorted(capitals.items(), key = lambda x: x[1]):
+    print(value)
