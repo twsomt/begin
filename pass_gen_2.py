df0dbbd642f65f990d835affe2fea05f940c2fde
twsomt <volk0v-ak@yandex.ru>
2022-05-17 20:49:52 +0300

    correct text

--- pass_gen_2.py
+++ pass_gen_2.py
@@ -1,5 +1,5 @@
 # принимает на вход длину пароля и оп-оп генерит
 from string import ascii_letters, punctuation
 from random import sample
-length_pass = int(input('Какойдлины пароль сгенерировать?'))
+length_pass = int(input('Какой длины пароль сгенерировать?'))
 print(''.join(sample(ascii_letters + punctuation, length_pass)))
