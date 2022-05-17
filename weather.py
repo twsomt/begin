297aa80135962d75ae8e8b64a256193b95fdf8c5
twsomt <volk0v-ak@yandex.ru>
2022-05-18 00:16:21 +0300

    weather in saint p.

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 #при использовании cmder, колорама всё ломает.
-import colorama
-colorama.init()
+# import colorama
+# colorama.init()
 from requests import get
 url = 'http://wttr.in/Saint_Petersburg'
 x = get(url)
