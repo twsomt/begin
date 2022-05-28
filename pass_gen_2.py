24ada9d90d7ab3ca9e7a214626f92a6f42b6ea2b
twsomt <volk0v-ak@yandex.ru>
2022-05-29 02:46:37 +0300

    added clipboard

--- pass_gen_2.py
+++ pass_gen_2.py
@@ -1,5 +1,12 @@
 # принимает на вход длину пароля и оп-оп генерит
 from string import ascii_letters, punctuation
 from random import sample
-length_pass = int(input('Какой длины пароль сгенерировать?'))
-print(''.join(sample(ascii_letters + punctuation, length_pass)))
+from pandas import DataFrame
+
+length_pass = int(input('Какой длины пароль сгенерировать? [Максимум - 84]: '))
+password = ''.join(sample(ascii_letters + punctuation, length_pass))
+
+result = DataFrame([password])
+result.to_clipboard(index=False,header=False)
+
+print('Пароль скопирован в буфер обмена!')
