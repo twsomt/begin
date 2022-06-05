53e8d2d75167686d9911c86d40ab91051aa51003
twsomt <volk0v-ak@yandex.ru>
2022-06-06 01:43:57 +0300

    another easy task

--- for_easy_task.py
+++ for_easy_task.py
@@ -1,130 +1,81 @@
-# def generate_ip():
-#     from random import randint
-#     return f'{randint(100, 255)}.{randint(100, 255)}.{randint(0, 9)}.{randint(100, 255)}'
-
-# print(generate_ip())
-
-
-# from random import shuffle
-# matrix = [[1, 2, 3, 4],
-#           [5, 6, 7, 8],
-#           [9, 10, 11, 12],
-#           [13, 14, 15, 16]]
-
-# [shuffle(i) for i in matrix]
+# def map(function, items):
+#     result = []
+#     for item in items:
+#         result.append(function(item))
+#     return result
 
 
-# from random import shuffle 
-# word = list(input())
-# shuffle(word)
-# print(''.join(word))
-
-# from random import randint as r
-# temp = set()
-# while not len(temp) == 25:
-#     temp.add(str(r(1, 75)).ljust(3))
-
-# temp = list(temp)
-# temp[12] = '  0'
-
-# for i in range(0, 30, 5):
-#     print(*temp[i-5:i])
-    
+# def filter(function, items):
+#     result = []
+#     for item in items:
+#         if function(item):
+#             result.append(item)
+#     return result
 
 
-# from random import randint as r
-# bingo = [[str(r(1, 75)).ljust(3) for i in range(5)] for j in range(5)]
-# bingo[2][2] = ' 0 '
-# [print(*i) for i in bingo]
+# numbers = [77, 293, 28, 242, 213, 285, 71, 286, 144, 276, 61, 298, 280, 214, 156, 227, 228, 51, -4, 202, 58, 99, 270, 219, 94, 253, 53, 235, 9, 158, 49, 183, 166, 205, 183, 266, 180, 6, 279, 200, 208, 231, 178, 201, 260, -35, 152, 115, 79, 284, 181, 92, 286, 98, 271, 259, 258, 196, -8, 43, 2, 128, 143, 43, 297, 229, 60, 254, -9, 5, 187, 220, -8, 111, 285, 5, 263, 187, 192, -9, 268, -9, 23, 71, 135, 7, -161, 65, 135, 29, 148, 242, 33, 35, 211, 5, 161, 46, 159, 23, 169, 23, 172, 184, -7, 228, 129, 274, 73, 197, 272, 54, 278, 26, 280, 13, 171, 2, 79, -2, 183, 10, 236, 276, 4, 29, -10, 41, 269, 94, 279, 129, 39, 92, -63, 263, 219, 57, 18, 236, 291, 234, 10, 250, 0, 64, 172, 216, 30, 15, 229, 205, 123, -105]
+
+# print(sum(map(lambda x: x ** 2 ,filter(lambda x: len(str(abs(x))) == 2 and x % 7 == 0 ,numbers))))
 
 
-# from re import sub
-# from string import ascii_letters, digits
-# from random import sample
+# def func(elem1, elem2, elem3, elem4):
+#     return elem1 + elem2 + elem3 + elem4
 
 
-# x = ascii_letters + digits
-# y = sub(r'[lI1oO0]', '', x)
-# print(x)
-# print(y)
+# numbers1 = [1, 2, 3, 4]
+# numbers2 = [10, 20, 30]
+# numbers3 = [100, 200, 300, 400, 500]
+# numbers4 = [100, 200, 300, 400, 500]
 
-# from fractions import Fraction
+# new_numbers = list(map(func, numbers1, numbers2, numbers3, numbers4))  #  преобразуем итератор в список
 
-# s = '0.78 4.3 9.6 3.88 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.13 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50 0.123 0.00021'
-# x = min(map(Fraction, s.split()))
-# y = max(map(Fraction, s.split()))
-
-# print(x + y)
+# print(new_numbers)
 
 
-# def func(qty): return {chr(96 + i):i for i in range(1, qty + 1)}
-# print(func(3))
-
-# def fancy(length, char1='-', char2='*'):
-#     return (char1 + char2) * length + char1
+# x = 56466464644646
+# print(f'{x}')
 
 
-# print(fancy(char2='!'))
 
-# x = ('Timur', 'Roman', 'Ruslan')
-# print(' and '.join(x))
+# numbers = [-2, 0, 1, 2, 17, 4, 5, 6]
+# result = list(map( lambda x: 'even' if x % 2 == 0 else 'add',numbers))
 
+# print(numbers)
+# print(result)
 
-# def greet(first_name, *args):
-#     return ' and '.join(args)
+# from functools import reduce
 
-# print(greet('Timur', 'Roman', 'Ruslan'))
-# #Hello, Timur and Roman and Ruslan!
+# numbers = range(10)
+# obj = map(lambda x: x + 1, numbers)
+# obj = filter(lambda x: x % 2 == 1, obj)
+# result = reduce(lambda x, y: x + y, obj, 0)
 
-# def print_products(*args):
-#     temp = [i for i in args if type(i) is str and i]
-#     if temp:
-#         for i in range(1, len(temp) + 1):
-#             print(f'{i}) {temp[i-1]}')
-#     else:
-#         print('Нет продуктов')
-
-# print_products([4], {}, 1, 2, {'Beegeek'}, '') 
-
-# def info_kwargs(**kwargs):
-#     for k, v in sorted(kwargs.items()):
-#         print(f'{k}: {v}')
-
-# info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher') 
+# print(result)
 
 
-# def f1(x):
-#     return 2*x+1
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
 
 
-# def f2(x):
-#     return x**2
-
-
-# def f3(x):
-#     return -x**2+1
-
-
-# def f4(x):
-#     return x-3
-
-
-# funcs = [f1, f2, f3, f4]
-# i = 4
-# print(funcs[i](2))
-
-# from math import sqrt, pow
-# points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
-
-# def coords(i):
-#     return sqrt(pow(i[0], 2) + pow(i[1], 2))
-
-# print(sorted(points, key=coords))
-
-
-nums = tuple('111 14 79 7 4 123 90 45 12 171'.split())
-
-def sorter(i):
-    return sum(map(int, i)), int(i)
-
-print(*sorted(nums, key=sorter))
+res = ', '.join(sorted(list((map(lambda x: x[0], (filter(lambda x: x[1] > 10000000 and x[-1] == 'primary', data)))))))
+print(f'Cities: {res}')
