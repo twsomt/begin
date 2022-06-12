8e136bb5cb8c35e2aa2469ed607fe21be3b33abc
twsomt <volk0v-ak@yandex.ru>
2022-06-12 20:05:19 +0300

    lambda

--- for_easy_task.py
+++ for_easy_task.py
@@ -1,13 +1,14 @@
-fib = lambda x : 1 if x <= 2 else fib(x - 1) + fib(x - 2)
-print(fib(31))
+print(all(lambda x: x.isnumeric() for x in 'aa.bb.cc.dd'.split('.')))
+# True
+
+print(all(x.isnumeric() for x in 'aa.bb.cc.dd'.split('.')))
+# True
+
+x = 'ab'
+print(list(map(lambda x: x.isnumeric(), 'aa.bb.cc.dd'.split('.'))))
+# [False, False, False, False]
 
 
-numbers = [1, 2, 3]
-words = ['one', 'two', 'three']
 
-for pair in zip(numbers, words):
-    print(pair)
-
-
-x = zip(numbers, words)
-print(list(x))
+h = 'fff'
+print(lambda x: x.isnumeric())
