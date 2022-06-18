ed157e5e9a357c90f0ae03832bbc003d6f097aea
twsomt <volk0v-ak@yandex.ru>
2022-06-18 23:26:40 +0300

    correct file

--- test_files_23/reader_writer.py
+++ test_files_23/reader_writer.py
@@ -1,5 +1,3 @@
-print('First')
-print('Second')
-print('Third')
-
-print(f'First\nSecond\nThird\n')
+with open('input.txt', 'r', encoding='utf-8') as read_file, open('output.txt', 'w', encoding='utf-8') as out_file:
+    for i, row in enumerate(read_file.readlines(), 1):
+        print(f'{i}) {row.strip()}', file=out_file)
