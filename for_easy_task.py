e6c53d14d5b84f800d0dbab18f39952a71091e05
twsomt <volk0v-ak@yandex.ru>
2022-06-18 16:20:47 +0300

    read csv

--- for_easy_task.py
+++ for_easy_task.py
@@ -1,14 +1,3 @@
-print(all(lambda x: x.isnumeric() for x in 'aa.bb.cc.dd'.split('.')))
-# True
-
-print(all(x.isnumeric() for x in 'aa.bb.cc.dd'.split('.')))
-# True
-
-x = 'ab'
-print(list(map(lambda x: x.isnumeric(), 'aa.bb.cc.dd'.split('.'))))
-# [False, False, False, False]
-
-
-
-h = 'fff'
-print(lambda x: x.isnumeric())
+error_message = 'ER03:"Ошибка подключения устройства?!??!?!! Доступ блокирован!"'
+text = error_message.strip('"E3!0R:')
+print(text)
