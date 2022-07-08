f07285194573f9661518a981d65b35cc3f680192
twsomt <volk0v-ak@yandex.ru>
2022-07-08 21:58:57 +0300

    Another oop task

---
+++ oop/easy_oop_17.py
@@ -0,0 +1,32 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_person_info(self):
+        print(f'Person: {self.name}, {self.age}')
+
+
+class Company:
+    def __init__(self, company_name, location):
+        self.company_name = company_name
+        self.location = location
+
+    def display_company_info(self):
+        print(f'Company: {self.company_name}, {self.location}')
+
+
+class Employee:
+    def __init__(self, name, age, company_name, location):
+        self.personal_data = Person(name, age)
+        self.work = Company(company_name, location)
+
+
+emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+print(emp.personal_data.name)
+print(emp.personal_data.age)
+emp.personal_data.display_person_info()
+print(emp.work.company_name)
+print(emp.work.location)
+emp.work.display_company_info()
+
