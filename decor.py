3abc5c265571bc39bfc2bf001f1d3f28cba99cc1
twsomt <volk0v-ak@yandex.ru>
2022-09-11 19:52:59 +0300

    easy decorator (or no)

--- decor.py
+++ decor.py
@@ -1,16 +1,10 @@
-global_variable = 'Глобальная'
+def say_saimon(func):
+    x = 'Саймон сказал:'
+    return f'{x} {func()}'
 
+@say_saimon
+def hi():
+    return 'привет'
 
-def some_func(passed_variable):
-    local_variable = 'Локальная'
+print(hi)
 
-    def inside_func():
-        inside_local_variable = 'Внутренняя'
-        return(f'{global_variable} '
-               f'{local_variable} '
-               f'{passed_variable} '
-               f'{inside_local_variable}')
-print(inside_func())
-
-some_func('Параметр')
-# Будет намечатано: Глобальная Локальная Параметр Внутренняя
