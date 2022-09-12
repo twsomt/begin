cd277a5b8687abeaae5fda98a9ff2c78521df20e
twsomt <volk0v-ak@yandex.ru>
2022-09-13 00:24:43 +0300

    not easy 37 oop 90%

--- oop/easy_oop_37.py
+++ oop/easy_oop_37.py
@@ -1,11 +1,27 @@
+import sys
 class ListObject:
-    def __init__(next_obj=None, data):
-        self.next_obj = next_obj
-        self.data = data
+    head_obj = None
+
+    def __init__(self, data, next_obj=None):
+            self.data = data
+            self.next_obj = next_obj
 
     def link(self, obj):
-        self.next_obj = obj
+        if not self.head_obj:
+            self.head_obj = ListObject(obj)
+        else:
+            self.data = obj
+        node = self.head_obj
 
-lst_in = list(map(str.strip, sys.stdin.readlines()))
+        while node.next_obj:
+            node = node.next_obj
 
-#some_code
+        node.next_obj = ListObject(obj)
+
+# lst_in = list(map(str.strip, sys.stdin.readlines())) 
+lst_in = list(range(10))
+print(*lst_in)
+
+for lst_elem in lst_in:
+    head_obj = ListObject(lst_elem)
+    print(head_obj.data, end=' ')
