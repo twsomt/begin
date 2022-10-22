f3501358bffb9424741c825ae8e3b0f5440e0d13
twsomt <volk0v-ak@yandex.ru>
2022-10-22 15:03:30 +0300

    Some task

--- codewars/sort_Strings_mix.py
+++ codewars/sort_Strings_mix.py
@@ -7,38 +7,11 @@ x = [
 ('z', 2, '='),
 ('a', 2, '=')]
 
-# print(x)
-x = sorted(x, key=lambda x: x[1], reverse=True)
-# print(x)
+def foo(tpl):
+    if tpl[2] == '=':
+        return -tpl[1], 0, tpl[0]
+    return -tpl[1], tpl[2], tpl[0]
 
 
-x_res = []
-n = len(set([i[1] for i in x]))
-
-for _ in range(n):
-    temp = []
-    y = x[0][1]
-    yy = 0
-    cnt = 0
-    while True:
-        yy = y
-        try:
-            y = x[cnt][1]
-        except:
-            break
-        
-        if y != x[0][1]:
-            break
-        
-        temp.append(x[cnt])
-        cnt += 1
-        
-    temp_1 = sorted([i for i in temp if i[-1] != '='], key=lambda x: (x[-1], x[0]))
-    temp_2 = sorted([i for i in temp if i[-1] == '='], key=lambda x: (x[-1], x[0]))
-    temp = temp_1 + temp_2
-    x_res += temp
-    
-    x = [i for i in x if i[1] != yy]
-
-
-[print(i) for i in x_res]
+res = sorted(x, key=foo)
+print(*res, sep='\n')
