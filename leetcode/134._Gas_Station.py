ba573acc7a121d78357281c15d2c116f06735306
twsomt <volk0v-ak@yandex.ru>
2023-01-07 18:10:48 +0300

    daily 7

---
+++ leetcode/134._Gas_Station.py
@@ -0,0 +1,28 @@
+'''
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station
+to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the countering gas station's index if you can travel
+around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution,
+it is guaranteed to be unique
+'''
+
+gas = [2, 3, 4]
+cost = [3, 4, 3]
+tank = 0
+counter = 0
+
+for i in range(len(gas)):
+    tank += gas[i]
+    tank -= cost[i]
+
+    if tank < 0:
+        tank = 0
+        counter = i + 1
+
+if (sum(gas) - sum(cost)) < 0:
+    print(-1)
+
+print(counter)
